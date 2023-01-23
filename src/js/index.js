0c64f634214b0ac6c9c06955f3f00021c3729814
andrealvesfilho <123349813+andrealvesfilho@users.noreply.github.com>
2023-01-23 02:26:19 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,23 @@
+console.log('mostrar o document', document);
+
+const botaoTrailer = document.querySelector(".botao-trailer");
+const video = document.getElementById("video")
+const modal = document.querySelector(".modal");
+const linkDoVideo = video.src;
+
+function alternarModal () {
+    modal.classList.toggle("aberto");
+}
+
+botaoTrailer.addEventListener("click", () => {
+    alternarModal();
+    video.setAttribute("src", linkDoVideo)
+});
+
+const botaoFecharModal = document.querySelector(".fechar-modal")
+
+botaoFecharModal.addEventListener("click", () => {
+    alternarModal();
+    video.setAttribute("src", "")
+})
+
